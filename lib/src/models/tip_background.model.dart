e01f843721263140285806ff3ff7e3301f73d31c
Morgan <mrgnhnt96@gmail.com>
2021-07-15 17:02:52 -0500

    refactor: created tip background

---
+++ lib/src/models/tip_background.model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'touch_through.model.dart';
+
+class TipBackground {
+  TipBackground({
+    this.color = Colors.white,
+    this.outsideColor = const Color.fromARGB(50, 255, 255, 255),
+    this.dismissible = true,
+    this.absorbPointerEvents = true,
+    this.containsOverlay = true,
+    this.touchThrough,
+  });
+
+  ///
+  /// The backgroundcolor of the Tooltip
+  final Color color;
+
+  /// The color of the rest of the overlay surrounding the Tooltip.
+  /// typically a translucent color.
+  final Color outsideColor;
+
+  ///
+  /// Allow the tooltip to be dismissed tapping outside
+  final bool dismissible;
+
+  ///
+  /// Block pointer actions or pass them through background
+  final bool absorbPointerEvents;
+
+  ///
+  /// Enable background overlay
+  final bool containsOverlay;
+
+  ///
+  /// By default touching the surrounding of the Tooltip closes the tooltip.
+  /// you can define a rectangle area where the background is completely transparent
+  /// and the widgets below react to touch
+  final TouchThrough? touchThrough;
+}
