10bfe8481e09143b56e8a0425b67c062e34b6934
Morgan <mrgnhnt96@gmail.com>
2021-07-16 13:19:40 -0500

    refactor: organized imports

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,6 +1,6 @@
-export 'absolute_position.model.dart';
-export 'absolute_position.model.dart';
 export 'border_decoration.model.dart';
 export 'pointer_decoration.model.dart';
 export 'tip_background.model.dart';
+export 'tip_position.model.dart';
+export 'tip_position.model.dart';
 export 'touch_through.model.dart';
