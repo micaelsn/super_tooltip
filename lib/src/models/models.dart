98824f6cb94d79a8417eed79bc08ef82e0225d0f
Morgan <mrgnhnt96@gmail.com>
2021-07-16 17:31:03 -0500

    refactor: added tip constraints to model

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,6 +1,6 @@
 export 'border_decoration.model.dart';
 export 'pointer_decoration.model.dart';
 export 'tip_background.model.dart';
-export 'tip_position.model.dart';
+export 'tip_constraints.model.dart';
 export 'tip_position.model.dart';
 export 'touch_through.model.dart';
