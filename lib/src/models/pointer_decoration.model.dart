58a28f072598cd5790b285cf43080348e821a34c
Morgan <mrgnhnt96@gmail.com>
2021-07-16 17:28:18 -0500

    refactor: added tip radius

--- lib/src/models/pointer_decoration.model.dart
+++ lib/src/models/pointer_decoration.model.dart
@@ -6,16 +6,18 @@ class PointerDecoration {
 
     ///
     /// The height of the pointer
-    this.distanceFromCenter = 20,
+    this.height = 20,
 
     ///
     /// The distance of the tip of the arrow's tip to the center of the target
-    this.height = 2,
+    this.distanceFromCenter = 2,
+    this.tipRadius = 2,
   });
 
   final double baseWidth;
-  final double distanceFromCenter;
   final double height;
+  final double distanceFromCenter;
+  final double tipRadius;
 
-  double get distanceAway => distanceFromCenter + height;
+  double get distanceAway => height + distanceFromCenter;
 }
