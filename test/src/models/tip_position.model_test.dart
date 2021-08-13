0c3e4943b860e3c409ff41da804f71ff3549f3de
Morgan <mrgnhnt96@gmail.com>
2021-08-13 09:47:50 -0500

    feat(test): added test for top position

---
+++ test/src/models/tip_position.model_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:super_tooltip/super_tooltip.dart';
+
+// set up
+// act
+// assert
+
+void main() {
+  group(
+    '$TipPosition#snapsHorizontal',
+    () {
+      test(
+        'should return true when snapTo is horizontal',
+        () {
+          // set up
+          final tipPosition = TipPosition.snap(SnapAxis.horizontal);
+          // assert
+          expect(tipPosition.snapsHorizontal, isTrue);
+        },
+      );
+
+      test(
+        'should return true when left & right are 0',
+        () {
+          // set up
+          final tipPosition = TipPosition(
+            left: 0,
+            right: 0,
+          );
+          // assert
+          expect(tipPosition.snapsHorizontal, isTrue);
+        },
+      );
+    },
+  );
+}
